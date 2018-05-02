cf6b3367afb3abf292d009534b50a21807488a32
Henry Tejera <henrytejera@gmail.com>
2018-05-02 01:56:18 -0300

    Added API doc

--- src/main/java/com/mobilebox/reader/core/Conts.java
+++
@@ -1,9 +0,0 @@
-package com.mobilebox.reader.core;
-
-public abstract class Conts {
-
-	public static final String CONTENT_TYPE_JSON = "application/json";
-	public static final String CONTENT_TYPE_HTML = "text/html";
-	public static final String CONTENT_TYPE_TEXT = "text/plain";
-	public static final String API_VERSION = "api/v1";
-}
